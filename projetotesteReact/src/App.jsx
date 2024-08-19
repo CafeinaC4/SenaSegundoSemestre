69dc2419387a801cbea928758fefdb90e01a884d
CafeinaC4 <vitordanielli3113@gmail.com>
2024-08-19 17:07:47 -0300

    Alteração da base do arquivo para teste, adicionado react

---
+++ projetotesteReact/src/App.jsx
@@ -0,0 +1,14 @@
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className='titulo'> Olá react</h1>
+    <h2>Texto Genérico 1.0</h2>
+    </>
+  )
+}
+
+export default App
